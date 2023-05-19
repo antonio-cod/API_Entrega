cc0bf137bdffe951cf18bdfa914bb54137260e27
Antonio-cod <antonio_carlostads@hotmail.com>
2023-05-18 21:52:22 -0400

    Usando Jakarta Persistence JPA

--- alalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ alalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,8 +1,25 @@
 package com.algaworks.algalog.api.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar(){
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+	}
 
 }
--- alalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ alalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,5 +1,26 @@
 package com.algaworks.algalog.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@Entity
 public class Cliente {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private String telefone;
 
 }
